88ac502849bab344421c0050a94a044036bd9776
georgesalmi <gmsalmi@icloud.com>
2024-08-20 19:13:25 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabId) {
+    var i, tabcontent, tabbuttons;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+    }
+    document.getElementById(tabId).style.display = "block";
+    evt.currentTarget.className += " active";
+}
